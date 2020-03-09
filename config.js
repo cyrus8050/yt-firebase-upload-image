a3f634ee8a65e1fd0c41f0aae8c8d8c58c579bc1
cyrusonline <online_mad@yahoo.com.hk>
2020-03-09 10:32:14 +0800

    init

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAyqv9OvgJOExsyuLOwvsi_OPktOrQpKls",
-    authDomain: "uploadphotos-d984a.firebaseapp.com",
-    databaseURL: "https://uploadphotos-d984a.firebaseio.com",
-    projectId: "uploadphotos-d984a",
-    storageBucket: "uploadphotos-d984a.appspot.com",
-    messagingSenderId: "198709055562",
-    appId: "1:198709055562:web:a1bcc813c819aeaa63d6b3"
+    //FIREBASE CONFIG
   };
 
 
